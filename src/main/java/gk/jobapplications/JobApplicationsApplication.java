5cb593c64065fdde5b1d0da5f94a14ae3518d85c
Gabriel Kunst <gabrielkt.dev@gmail.com>
2024-08-14 19:44:34 -0300

    chore: add project setup

---
+++ src/main/java/gk/jobapplications/JobApplicationsApplication.java
@@ -0,0 +1,13 @@
+package gk.jobapplications;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobApplicationsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JobApplicationsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/gk/jobapplications/JobApplicationsApplicationTests.java
@@ -0,0 +1,13 @@
+package gk.jobapplications;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobApplicationsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
