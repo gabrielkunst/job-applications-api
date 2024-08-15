4abbf147884373962649d3457e72198ba7eca28c
JLisarte <juliano.lisarte@gmail.com>
2024-08-15 09:24:31 -0300

    feat: add package controllers, entities, repositories, services

--- src/main/java/gk/jobapplications/entities/CandidateEntity.java
+++ src/main/java/gk/jobapplications/entities/CandidateEntity.java
@@ -1,18 +1,30 @@
 package gk.jobapplications.entities;
 
-import java.time.LocalDateTime;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table
+@Entity(name = "candidate")
 public class CandidateEntity {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
     private String name;
-
     private String email;
-
     private String profession;
-
     private String passwordHash;
-
     private LocalDateTime createdAt;
-
     private LocalDateTime deletedAt;
 }
